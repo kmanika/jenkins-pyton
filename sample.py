b155d85c6c4a8a9e3355e65fd0fc64c9b4266c4b
kmanika <kommuri.mani@gmail.com>
2022-05-24 13:05:47 +0530

    commiting changes

--- sample.py
+++ sample.py
@@ -2,3 +2,4 @@ import jenkins
 server = jenkins.Jenkins('http://localhost:8080')
 version = server.get_version()
 print('my version: ', version)
+server.create_job('folder',jenkins.EMPTY_FOLDER_XML)
