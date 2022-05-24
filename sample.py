792d177b55641551fe6fbab795669d7ebf630aa0
kmanika <kommuri.mani@gmail.com>
2022-05-24 13:06:36 +0530

    commiting changes

--- sample.py
+++ sample.py
@@ -2,4 +2,4 @@ import jenkins
 server = jenkins.Jenkins('http://localhost:8080')
 version = server.get_version()
 print('my version: ', version)
-server.create_job('folder',jenkins.EMPTY_FOLDER_XML)
+server.create_job('aws-lambda',jenkins.EMPTY_FOLDER_XML)
